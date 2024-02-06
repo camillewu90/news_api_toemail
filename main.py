db716c9bbaca246b6853cf0858f6e3ce1e5a8e48
camillewu90 <51872168+camillewu90@users.noreply.github.com>
2024-02-06 13:13:26 -0800

    Add links, specify language, limit news count, control the category

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 import send_email
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-01-06\
-&sortBy=publishedAt&apiKey=61aa060bbb4e4a6cbe56cc8ac2b599aa"
-api_key = "61aa060bbb4e4a6cbe56cc8ac2b599aa"
+topic = "tesla"
 
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2024-01-06&" \
+      "sortBy=publishedAt&" \
+      "apiKey=61aa060bbb4e4a6cbe56cc8ac2b599aa&" \
+      "language=en"
+api_key = "61aa060bbb4e4a6cbe56cc8ac2b599aa"
 
 # Make a request
 request = requests.get(url)
@@ -20,18 +25,19 @@ message = """Subject: news digest\n
 """
 
 # Populate the article title and description in the message
-for article in articles:
+for article in articles[:20]:
     if article["title"]:
         message += article["title"] + "\n"
     else:
         message += "Missing title" + "\n"
 
     if article["description"]:
-        message += article["description"] + 2*"\n"
+        message += article["description"] + "\n"
     else:
-        message += "Missing description" + 2*"\n"
+        message += "Missing description" + "\n"
+
+    message += article["url"] + 2 * "\n"
 
 message = message.encode('utf-8')
 # Send email of news digest
 send_email.send_email(message)
-
